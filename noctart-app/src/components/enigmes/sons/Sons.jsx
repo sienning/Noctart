3052731f5be5b3b54185578f8a915c1824c9c707
sienning <sienning@hotmail.fr>
2023-01-10 14:17:39 +0100

    camera mobile

--- noctart-app/src/components/enigmes/sons/Sons.jsx
+++ noctart-app/src/components/enigmes/sons/Sons.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Recup from "./Recup";
 import Rendre from "./Rendre";
+import { Camera } from "react-camera-pro";
 
 const Sons = () => {
+    const camera = useRef(null);
+    const [image, setImage] = useState(null);
+    const [numberOfCameras, setNumberOfCameras] = useState(0);
     const [isRecup, setIsRecup] = useState(false);
     const [isRendre, setIsRendre] = useState(false);
+    const [devices, setDevices] = useState([]);
+    const [activeDeviceId, setActiveDeviceId] = useState(undefined);
 
     const handleRecup = () => {
         setIsRecup(true)
@@ -16,16 +22,45 @@ const Sons = () => {
         setIsRendre(true)
     }
 
+    useEffect(() => {
+        (async () => {
+            const devices = await navigator.mediaDevices.enumerateDevices();
+            const videoDevices = devices.filter(i => i.kind == 'videoinput');
+            setDevices(videoDevices);
+            if (videoDevices.length === 1) setActiveDeviceId(videoDevices[0].deviceId)
+        })();
+    });
+
+
     return (
         <div className="Sons">
             <div>
                 <h1>Sons des tableaux</h1>
-                {
-                    isRecup && <Recup />
-                }
-                {
-                    isRendre && <Rendre />
-                }
+                <div
+                    className="camera-div"
+                >
+                    <Camera
+                        ref={camera}
+                        numberOfCamerasCallback={setNumberOfCameras}
+                        facingMode='environment'
+                        aspectRatio="cover"
+                        videoSourceDeviceId={activeDeviceId}
+                    />
+                </div>
+                <span>{activeDeviceId}</span>
+                <select
+                    onChange={event => {
+                        setActiveDeviceId(event.target.value);
+                    }}
+                >
+                    {devices.map(d => (
+                        <option key={d.deviceId} value={d.deviceId}>
+                            {d.label}
+                        </option>
+                    ))}
+                </select>
+                { isRecup && <Recup /> }
+                { isRendre && <Rendre /> }
                 <div>
                     <button onClick={handleRecup} style={{ marginRight: 10 }}>Récupérer un son</button>
                     <button onClick={handleRendre}>Rendre un son</button>
--- noctart-app/src/main.jsx
+++ noctart-app/src/main.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />,
 )
+
+serviceWorker.unregister();
